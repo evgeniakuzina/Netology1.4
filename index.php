d887f5bf16c44ce9034f1813e1312954d0e6d5a4
evgeniakuzina <30548350+evgeniakuzina@users.noreply.github.com>
2018-05-24 13:15:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <?php
 
-$json = file_get_contents("http://api.openweathermap.org/data/2.5/find?q={$_GET['q']}&units=metric&appid=af16a194ec280290e5159489319cdd51");
-$data = json_decode($json, true);
+require("functions.php");
+
 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Weather forecast for <?php echo $data["list"][0]["name"] ?></title>
+	<title>Weather forecast</title>
 </head>
 <body>
-	<p>Today in <?php echo $data["list"][0]["name"] ?>: <?php echo $data["list"][0]["weather"][0]["description"] ?>, temperature: <?php echo $data["list"][0]["main"]["temp"] ?> C.</p>
+    <?php if (isset($weather)): ?>
+        <p><?php echo($weather); ?></p>
+    <?php else: ?>
+    <h1>City:</h1>
+    <form action="" method="POST">
+        <input type="text" name="q">
+        <input type="submit" value="Search">
+    </form>
+    <?php endif; ?>
 </body>
 </html>
